5f091ebedc4bf8b4d0eacfa20942949924274e4b
jiangdajie <18261151128@163.com>
2018-07-09 14:28:53 +0800

    合并分支

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -3,7 +3,15 @@ package com.atguigu.git;
 public class Apple {
 	public static void main(String[] args) {
 		System.out.println("hot_fix......");
-		
+
 		System.out.println("在hot_fix分支上commit");
+
+		System.out.println("IDEA commit test...");
+		System.out.print("pull test");
+		System.out.println("commit test2");
+		System.out.println("pull2");
+		System.out.println("commit 3...");
+		System.out.println("commit eclipse");
+
 	}
 }
