9ec82828062ada533acba5ce05874091a45d34ca
jiangdajie <40766206+jiangdajie@users.noreply.github.com>
2018-07-09 09:36:29 +0800

    Update Apple.java

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -5,6 +5,7 @@ public class Apple {
 		System.out.println("hot_fix......");
 		System.out.println("IDEA commit test...");
 		System.out.print("pull test");
-        System.out.println("commit test2");
+        	System.out.println("commit test2");
+		System.out.println("pull2");
 	}
 }
