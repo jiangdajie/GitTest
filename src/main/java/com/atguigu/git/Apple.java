4bf24cfc27b896b81b438f152a1596da0262af6a
jiangdajie <18261151128@163.com>
2018-07-09 14:21:47 +0800

    在Hot_fix删固体胶

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -3,5 +3,7 @@ package com.atguigu.git;
 public class Apple {
 	public static void main(String[] args) {
 		System.out.println("hot_fix......");
+		
+		System.out.println("在hot_fix分支上commit");
 	}
 }
