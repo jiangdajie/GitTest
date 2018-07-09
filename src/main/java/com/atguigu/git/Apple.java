de72210dd7cdb98c381aef8fe7837894a0e06f93
jiangdajie <18261151128@163.com>
2018-07-09 09:37:24 +0800

    commit first test

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -5,7 +5,8 @@ public class Apple {
 		System.out.println("hot_fix......");
 		System.out.println("IDEA commit test...");
 		System.out.print("pull test");
-        	System.out.println("commit test2");
+		System.out.println("commit test2");
 		System.out.println("pull2");
+		System.out.println("commit 3...");
 	}
 }
